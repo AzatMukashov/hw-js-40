47dc5290cfab97edc844e2f393d05ff86206ea33
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-18 18:13:06 +0600

    добавил хранилище

---
+++ src/app/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contactReducer from '../components/contactReducer.ts';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactReducer,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
