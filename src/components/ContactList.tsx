3bc944ab85ae719fe3a4427b5c40aa052b68a2fd
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-18 18:16:16 +0600

    создал компонент главной страницы

---
+++ src/components/ContactList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Contact } from '../types';
+
+interface ContactListProps {
+  contacts: Contact[];
+  openContactModal: (contact: Contact) => void;
+  addNewContact: () => void;
+}
+
+const ContactList: React.FC<ContactListProps> = ({contacts, openContactModal, addNewContact}) => {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <button onClick={addNewContact}>Add new contact</button>
+      <ul>
+        {contacts.map((contact, index) => (
+          <li key={index} onClick={() => openContactModal(contact)} style={{display: 'flex', alignItems: 'center'}}>
+            <img
+              src={contact.photo}
+              alt={`${contact.name}'s photo`}
+              style={{
+                width: '50px',
+                height: '50px',
+                borderRadius: '50%',
+                marginRight: '10px'
+              }}
+            />
+            {contact.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ContactList;
