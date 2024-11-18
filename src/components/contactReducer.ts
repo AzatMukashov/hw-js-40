da1ce920adcb21eac9380a748010817a5c0732e0
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-18 18:14:46 +0600

    создал файл для управления состояниями

---
+++ src/components/contactReducer.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Contact } from '../types';
+
+interface ContactState {
+  contacts: Contact[];
+}
+
+const initialState: ContactState = {
+  contacts: [],
+};
+
+const contactSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, action: PayloadAction<Contact>) => {
+      state.contacts.push(action.payload);
+    },
+    editContact: (state, action: PayloadAction<Contact>) => {
+      const index = state.contacts.findIndex((contact) => contact.id === action.payload.id);
+      if (index !== -1) {
+        state.contacts[index] = action.payload;
+      }
+    },
+    deleteContact: (state, action: PayloadAction<string>) => {
+      state.contacts = state.contacts.filter((contact) => contact.id !== action.payload);
+    },
+    setContacts: (state, action: PayloadAction<Contact[]>) => {
+      state.contacts = action.payload;
+    },
+  },
+});
+export const {addContact, editContact, deleteContact, setContacts} = contactSlice.actions;
+export default contactSlice.reducer;
