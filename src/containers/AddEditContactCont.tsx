bb667f71794db8d9aee60e73394bdf36212da3d3
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-18 18:23:12 +0600

    Очень спешил так что - где написано файл значит компонент. Здесь получение параметров, отправление действий для обновления, получение текущего контакта

---
+++ src/containers/AddEditContactCont.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../app/store.ts';
+import { addNewContact, fetchContacts, updateContact } from '../components/contactAction.ts';
+import AddEditContact from '../components/AddEditContact.tsx';
+import { Contact } from '../types';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const AddEditContactCont: React.FC = () => {
+  const {id} = useParams<{id: string}>();
+  const dispatch: AppDispatch = useDispatch();
+  const contacts = useSelector((state: RootState) => state.contacts.contacts);
+  const currentContact = id ? contacts.find(contact => contact.id === id) : undefined;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!contacts.length) {
+      dispatch(fetchContacts());
+    }
+  }, [dispatch, contacts]);
+  const handleSubmit = (newContact: Contact) => {
+    if (currentContact) {
+      dispatch(updateContact(newContact));
+    } else {
+      dispatch(addNewContact(newContact));
+    }
+    navigate('/');
+  };
+  return (
+    <AddEditContact contact={currentContact} onSubmit={handleSubmit}/>
+  );
+};
+
+export default AddEditContactCont;
