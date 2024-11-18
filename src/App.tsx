3d9e2490f7c68887f74e55076b2d7b57569ab8e7
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-11-18 18:23:56 +0600

    здесь происходит маршрутизация

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactListCont from './containers/ContactListCont.tsx';
+import AddEditContactCont from './containers/AddEditContactCont.tsx';
+
 
 const App = () => {
-
   return (
-    <>
-
-    </>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true}}>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<ContactListCont/>}/>
+          <Route path="/add-contact" element={<AddEditContactCont/>}/>
+          <Route path="/edit-contact/:id" element={<AddEditContactCont/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 };
 
